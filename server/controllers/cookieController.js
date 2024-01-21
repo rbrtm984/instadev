96c43a7548dae43739f9f7d97e8d5a770960eb0d
Rob Mayo <rbrtm984@gmail.com>
2024-01-21 17:09:14 -0500

    adding error handling and input validation to cookieController.js

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,27 +1,42 @@
 const cookieController = {};
 
-
 /**
 * setSSIDCookie - store the user id in a cookie
 */
 cookieController.setSSIDCookie = (req, res, next) => {
+  try {
+    // Validate input
+    if (typeof req.body.id !== 'string' || req.body.id.trim() === '') {
+      throw new Error('Invalid input');
+    }
 
-
-  res.cookie('ssid', `testvalue`, { maxAge: 30000 }, { httpOnly: true })
-
-  return next()
-}
-
-cookieController.checkCookie = (req, res, next) => {
-  const currSSID = req.cookies.ssid
-  console.log('curr cookie is', currSSID)
-
-  if (currSSID) {
-    return next()
-  } else {
-    res.redirect('http://localhost:3000/login')
+    res.cookie('ssid', req.body.id, { maxAge: 30000, httpOnly: true });
+    return next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while setting the cookie');
   }
 }
 
+cookieController.checkCookie = (req, res, next) => {
+  try {
+    const currSSID = req.cookies.ssid;
+    console.log('curr cookie is', currSSID);
+
+    // Validate cookie
+    if (typeof currSSID !== 'string' || currSSID.trim() === '') {
+      throw new Error('Invalid cookie');
+    }
+
+    if (currSSID) {
+      return next();
+    } else {
+      res.redirect('http://localhost:3000/login');
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('http://localhost:3000/login');
+  }
+}
 
 module.exports = cookieController;
